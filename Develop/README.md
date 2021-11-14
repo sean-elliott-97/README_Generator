91c494fd1d5690d5ba98a344d98e67d73a411c20
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-11-14 14:39:36 -0500

    initial upload

---
+++ Develop/README.md
@@ -0,0 +1,44 @@
+
+    # readme generator
+    
+    ![Mozilla Public License 2.0](https://img.shields.io/static/v1?label=License&message=Mozilla%20Public%20License%202.0&color=green)
+    
+    
+    ## Description
+    --- 
+    generates a readme
+    
+    ## Table of Contents
+    ---
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution](#contribution)
+    * [License](#license)
+    * [Test](#test)
+    * [Questions](#questions)
+    
+    ## Installation
+    ---
+    install node on computer, clone repo
+    
+    ## Usage
+    ---
+    answer all prompts, then view generated readme 
+    
+    ## Contribution
+    ---
+    can edit repo
+    
+    ## License
+    The license used in this project was Mozilla Public License 2.0. More information about this license can be found at [Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/).
+    
+    ---
+    ## Test
+    ---
+    test
+    
+    ## Questions
+    ---
+    My other work can be found at <a href="https://github.com/sean-elliott-97" title="github profile" target = "blank">sean-elliott-97</a>. If you have any additional questions, you can email me at [sean.elliott.33197](mailto:sean.elliott.33197).
+    
+    
---
+++ Develop/index.js
@@ -0,0 +1,151 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+//const { fileName, data } = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the name of your project? (Required)",
+    validate: (projectTitle) => {
+      if (projectTitle) {
+        return true;
+      } else {
+        console.log("Please enter a title for your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "projectDescription",
+    message: "Enter a description for your project (Required)",
+    validate: (projectDescription) => {
+      if (projectDescription) {
+        return true;
+      } else {
+        console.log("Please enter a description for your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions (Required)",
+    validate: (installation) => {
+      if (installation) {
+        return true;
+      } else {
+        console.log("Please enter the installation instructions!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information (Required)",
+    validate: (usageInfo) => {
+      if (usageInfo) {
+        return true;
+      } else {
+        console.log("Please enter the usage information!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines",
+    validate: (cont)=>{
+      if(cont){
+        return true;
+      }
+      else{
+        console.log("Please enter contribution guidelines!");
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions",
+    validate: (tInstructions)=>{
+      if(tInstructions){
+        return true;
+      }
+      else{
+        console.log("Please enter the test instructions!");
+        return false;
+      }
+    }
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license was used?",
+    choices: [
+      "NONE",
+      "GNU AGPLv3",
+      "GNU LGPLv3",
+      "Apache License 2.0",
+      "GNU GPLv2",
+      "GNU GPLv3",
+      "Mozilla Public License 2.0",
+      "MIT License",
+      "ISC License",
+      "Boost Software License 1.0",
+      "The Unlicense",
+    ],
+    
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub profile",
+    validate: (gProfile)=>{
+      if(gProfile){
+        return true;
+      }
+      else{
+        console.log("Please enter your GitHub profile!");
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+    validate: (eAddress)=>{
+      if(eAddress){
+        return true;
+      }
+      else{
+        console.log("Please enter your email address!");
+        return false;
+      }
+    }
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("README created");
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then(function (answers) {
+    writeToFile("README.md", generateMarkdown(answers));
+  });
+}
+
+//function call to initialize app
+init();
